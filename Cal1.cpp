7abedab9bdd81040daad1be1cdb39a338f0090ad
WoolfStore <hoplandbeer@mail.ru>
2018-12-16 21:25:04 +0300

    Добавили gitignore и сложение

---
+++ Cal1.cpp
@@ -0,0 +1,27 @@
+#include "StdAfx.h"
+#include <iostream>
+#include "plus.h"
+
+using namespace std;
+int main(){
+	double a;
+	double b;
+	char choise;
+	cout << "Vvedite 2 chisla: "<<endl;
+	cin >> a>> b;
+	do
+	{
+		cout << " Vvedite deistvie "<<endl;
+		cin >> choise;
+		switch (choise)
+		{
+		case ('+'): {
+			cout << Plus(a, b) << endl;
+			break;
+		}
+
+		}
+system("pause");
+return 0;
+}
+}
